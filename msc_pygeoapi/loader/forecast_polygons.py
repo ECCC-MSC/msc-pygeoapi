ee6098625a76b307704ec84db603dc2b0ca51989
Tom Kralidis <tomkralidis@gmail.com>
2020-11-19 10:33:05 -0500

    use logging settings from environment (#76)

--- msc_pygeoapi/loader/forecast_polygons.py
+++ msc_pygeoapi/loader/forecast_polygons.py
@@ -39,12 +39,12 @@ from osgeo import ogr
 from msc_pygeoapi import cli_options
 from msc_pygeoapi.env import (
     MSC_PYGEOAPI_ES_TIMEOUT, MSC_PYGEOAPI_ES_URL,
-    MSC_PYGEOAPI_ES_AUTH)
+    MSC_PYGEOAPI_ES_AUTH, MSC_PYGEOAPI_LOGGING_LOGLEVEL)
 from msc_pygeoapi.loader.base import BaseLoader
 from msc_pygeoapi.util import get_es, json_pretty_print
 
 LOGGER = logging.getLogger(__name__)
-elastic_logger.setLevel(logging.WARNING)
+elastic_logger.setLevel(getattr(logging, MSC_PYGEOAPI_LOGGING_LOGLEVEL))
 
 # index settings
 INDEX_NAME = 'forecast_polygons_{}_{}'
