f14c83efcbee1551281b28175d6f6af4e3b824b6
Tom Kralidis <tomkralidis@gmail.com>
2021-10-19 12:01:14 +0000

    update release version 0.9.13

--- msc_pygeoapi/__init__.py
+++ msc_pygeoapi/__init__.py
@@ -36,7 +36,7 @@ from msc_pygeoapi.log import setup_logger
 from msc_pygeoapi.process import process
 
 
-__version__ = '0.9.12'
+__version__ = '0.9.13'
 
 setup_logger(MSC_PYGEOAPI_LOGGING_LOGLEVEL, MSC_PYGEOAPI_LOGGING_LOGFILE)
 
