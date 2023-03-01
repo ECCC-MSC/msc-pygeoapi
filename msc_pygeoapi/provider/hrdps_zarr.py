18ab1c6b38049e6a7ef1530282a42f64c4a667f2
Adan Butt <afswadb@geomet-dev-03.cmc.ec.gc.ca>
2023-03-01 21:24:50 +0000

    no need for try/except

--- msc_pygeoapi/provider/hrdps_zarr.py
+++ msc_pygeoapi/provider/hrdps_zarr.py
@@ -438,11 +438,8 @@ def _gen_domain_axis(self, data):
     # Dynammically getting all of the axis names
     all_axis = []
     for coord in data.dims:
-        try:
             all_axis.append(coord)
-        except AttributeError:
-            LOGGER.warning(f'{coord} has no axis attribute but a coordinate.')
-            pass
+
     # Makes sure axis are in the correct order
     j, k = all_axis.index('lon'), all_axis.index(all_axis[0])
     all_axis[j], all_axis[k] = all_axis[k], all_axis[j]
