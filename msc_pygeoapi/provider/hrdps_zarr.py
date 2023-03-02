f9305322f4b68fa6b9dc08a6688a595410dc8399
Adan Butt <afswadb@geomet-dev-03.cmc.ec.gc.ca>
2023-03-02 11:26:17 +0000

    bbox extent

--- msc_pygeoapi/provider/hrdps_zarr.py
+++ msc_pygeoapi/provider/hrdps_zarr.py
@@ -299,13 +299,14 @@ class HRDPSWEonGZarrProvider(BaseProvider):
             if bbox:
                 # convert bbox projection
                 bbox = _convert_bbox_to_crs(bbox, self.crs)
+                
+                x_range = numpy.arange(self._coverage_properties['extent']['minx'],
+                                       self._coverage_properties['extent']['maxx'])
+                y_range = numpy.arange(self._coverage_properties['extent']['miny'],
+                                       self._coverage_properties['extent']['maxy'])
 
-                if any(
-                    [bbox[0] < self._coverage_properties['extent']['minx'],
-                     bbox[1] < self._coverage_properties['extent']['miny'],
-                     bbox[2] > self._coverage_properties['extent']['maxx'],
-                     bbox[3] > self._coverage_properties['extent']['maxy']]
-                ):
+                if all([bbox[0] not in x_range, bbox[2] not in x_range,
+                        bbox[1] not in y_range, bbox[3] not in y_range]):
                     msg = 'Invalid bbox (Values must fit coverage extent)'
                     LOGGER.error(msg)
                     raise ProviderInvalidQueryError(msg)
