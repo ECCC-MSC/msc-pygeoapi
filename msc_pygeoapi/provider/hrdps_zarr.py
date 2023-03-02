379eea67f6b160a89466acae1da949780dab317b
adanb13 <83403825+adanb13@users.noreply.github.com>
2023-03-02 10:26:06 -0500

    New extent fix (#244)
    * extent filter works and thorws 204
    
    * flake8
    
    * flake8
    
    * flake8
    
    * correct check for bbox before query and 204
    
    ---------
    
    Co-authored-by: Adan Butt <afswadb@geomet-dev-03.cmc.ec.gc.ca>

--- msc_pygeoapi/provider/hrdps_zarr.py
+++ msc_pygeoapi/provider/hrdps_zarr.py
@@ -300,18 +300,15 @@ class HRDPSWEonGZarrProvider(BaseProvider):
                 # convert bbox projection
                 bbox = _convert_bbox_to_crs(bbox, self.crs)
 
-                x_range = numpy.arange(
-                    self._coverage_properties['extent']['minx'],
-                    self._coverage_properties['extent']['maxx'])
-                y_range = numpy.arange(
-                    self._coverage_properties['extent']['miny'],
-                    self._coverage_properties['extent']['maxy'])
-
-                if all([bbox[0] not in x_range, bbox[2] not in x_range,
-                        bbox[1] not in y_range, bbox[3] not in y_range]):
+                if any([
+                    bbox[0] >= self._coverage_properties['extent']['maxx'],
+                    bbox[2] <= self._coverage_properties['extent']['minx'],
+                    bbox[1] >= self._coverage_properties['extent']['maxy'],
+                    bbox[3] <= self._coverage_properties['extent']['miny']
+                ]):
                     msg = 'Invalid bbox (Values must fit coverage extent)'
                     LOGGER.error(msg)
-                    raise ProviderInvalidQueryError(msg)
+                    raise ProviderNoDataError(msg)
                 elif 'lat' in query_return or 'lon' in query_return:
                     msg = (
                           'Invalid subset' +
