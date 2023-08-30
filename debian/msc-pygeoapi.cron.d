4b5fdfe25d15f4588e9d69c6ef14c8f5a37c2ac8
Etienne Pelletier <etienne.pelletier@canada.ca>
2023-08-30 17:11:07 +0000

    ensure geoadm .profile sourced prior to running commands

--- debian/msc-pygeoapi.cron.d
+++ debian/msc-pygeoapi.cron.d
@@ -29,25 +29,25 @@ MAILTO=""
 # =================================================================
 
 # every day at 0300h, clean bulletin records from ES
-0 3 * * * geoadm msc-pygeoapi data bulletins-realtime clean-indexes --days 140 --yes
+0 3 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data bulletins-realtime clean-indexes --days 140 --yes
 
 # every day at 0400h, clean hydrometric realtime data older than 30 days
-0 4 * * * geoadm msc-pygeoapi data hydrometric-realtime clean-indexes --days 30 --yes
+0 4 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data hydrometric-realtime clean-indexes --days 30 --yes
 
 # every hour on the 04, cache hydrometric stations
-4 * * * * geoadm msc-pygeoapi data hydrometric-realtime cache-stations
+4 * * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data hydrometric-realtime cache-stations
 
 # every day at 0500h, clean swob realtime data older than 30 days
-0 5 * * * geoadm msc-pygeoapi data swob-realtime clean-indexes --days 30 --yes
+0 5 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data swob-realtime clean-indexes --days 30 --yes
 
 # every day at 0600h, clean aqhi realtime data older than 3 days
-0 6 * * * geoadm msc-pygeoapi data aqhi-realtime clean-indexes --dataset all --days 3 --yes
+0 6 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data aqhi-realtime clean-indexes --dataset all --days 3 --yes
 
 # every day at 0700h, clean metnotes data older than 7 days
-0 7 * * * geoadm msc-pygeoapi data metnotes clean-indexes --days 7 --yes
+0 7 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data metnotes clean-indexes --days 7 --yes
 
 # every day at 0800h, clean ltce data older than 3 days
-0 8 * * * geoadm msc-pygeoapi data ltce clean-indexes --days 3 --yes
+0 8 * * * geoadm . /local/home/geoadm/.profile && msc-pygeoapi data ltce clean-indexes --days 3 --yes
 
 # every day at 0300h, clean out empty MetPX directories
-0 3 * * * geoadm /usr/bin/find $MSC_PYGEOAPI_CACHEDIR -type d -empty -delete > /dev/null 2>&1
+0 3 * * * geoadm . /local/home/geoadm/.profile && /usr/bin/find $MSC_PYGEOAPI_CACHEDIR -type d -empty -delete > /dev/null 2>&1
