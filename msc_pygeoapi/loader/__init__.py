125ae0436b3f18286e8ce4db2ad118d40ceea3d9
Etienne Pelletier <etienne.pelletier@canada.ca>
2025-01-09 19:54:54 +0000

    add mvt-elastic support for forecast regions and world collection

--- msc_pygeoapi/loader/__init__.py
+++ msc_pygeoapi/loader/__init__.py
@@ -73,7 +73,8 @@ commands = (
     ('msc_pygeoapi.loader.umos_realtime', 'umos_realtime'),
     ('msc_pygeoapi.loader.thunderstorm_outlook', 'thunderstorm_outlook'),
     ('msc_pygeoapi.loader.coastal_flood_risk_index',
-     'coastal_flood_risk_index')
+     'coastal_flood_risk_index'),
+    ('msc_pygeoapi.loader.naturalearth', 'naturalearth')
 )
 
 for module, name in commands:
