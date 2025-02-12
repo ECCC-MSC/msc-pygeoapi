5628ee453ede631ee28c9fb4f65f198fd7e605d7
Louis-Philippe Rousseau Lambert <louis-philippe.rousseaulambert@canada.ca>
2025-02-12 18:54:18 +0000

    initial implementation of the RDPA provider for the new OACov version
    removed empty line
    
    import update

--- msc_pygeoapi/provider/rdpa_rasterio.py
+++ msc_pygeoapi/provider/rdpa_rasterio.py
@@ -3,8 +3,8 @@
 # Authors: Louis-Philippe Rousseau-Lambert
 #           <louis-philippe.rousseaulambert@ec.gc.ca>
 #
-# Copyright (c) 2022 Louis-Philippe Rousseau-Lambert
 # Copyright (c) 2023 Tom Kralidis
+# Copyright (c) 2025 Louis-Philippe Rousseau-Lambert
 #
 #
 # Permission is hereby granted, free of charge, to any person
@@ -42,28 +42,28 @@ from rasterio.crs import CRS
 from rasterio.io import MemoryFile
 import rasterio.mask
 
-from pygeoapi.provider.base import (BaseProvider, ProviderConnectionError,
+from pygeoapi.provider.base import (BaseProvider,
+                                    ProviderConnectionError,
                                     ProviderQueryError)
+from pygeoapi.provider.rasterio_ import RasterioProvider
 
 LOGGER = logging.getLogger(__name__)
 
 
-# TODO: use RasterioProvider once pyproj is updated on bionic
-class RDPAProvider(BaseProvider):
+class RDPAProvider(RasterioProvider):
     """RDPA Provider"""
 
     def __init__(self, provider_def):
         """
         Initialize object
         :param provider_def: provider definition
-        :returns: pygeoapi.provider.cangrdrasterio.CanGRDProvider
+        :returns: pygeoapi.provider.rdpa_rasterio.RDPAProvider
         """
 
-        super().__init__(provider_def)
+        BaseProvider.__init__(self, provider_def)
 
         try:
             self.file_list = []
-
             pattern = 'CMC_RDPA_{}cutoff'
             self.var = search(pattern, self.data)[0]
             self.get_file_list(self.var)
@@ -90,161 +90,13 @@ class RDPAProvider(BaseProvider):
             self._data._crs = self.crs
             self._data._transform = self.transform
             self.num_bands = self._coverage_properties['num_bands']
-            self.fields = [str(num) for num in range(1, self.num_bands+1)]
+
+            self.get_fields()
             self.native_format = provider_def['format']['name']
 
-            # Needed to set the variable for each collection
-            # We intialize the collection matadata through this function
-            self.coverage = self.get_coverage_domainset()
         except Exception as err:
             LOGGER.warning(err)
             raise ProviderConnectionError(err)
-
-    # TODO: update domainset initialization with super
-    # once pyproj is updated on bionic
-    def get_coverage_domainset(self):
-        """
-        Provide coverage domainset
-
-        :returns: CIS JSON object of domainset metadata
-        """
-
-        domainset = {
-            'type': 'DomainSet',
-            'generalGrid': {
-                'type': 'GeneralGridCoverage',
-                'srsName': self._coverage_properties['bbox_crs'],
-                'axisLabels': [
-                    self._coverage_properties['x_axis_label'],
-                    self._coverage_properties['y_axis_label']
-                ],
-                'axis': [{
-                    'type': 'RegularAxis',
-                    'axisLabel': self._coverage_properties['x_axis_label'],
-                    'lowerBound': self._coverage_properties['bbox'][0],
-                    'upperBound': self._coverage_properties['bbox'][2],
-                    'uomLabel': self._coverage_properties['bbox_units'],
-                    'resolution': self._coverage_properties['resx']
-                }, {
-                    'type': 'RegularAxis',
-                    'axisLabel': self._coverage_properties['y_axis_label'],
-                    'lowerBound': self._coverage_properties['bbox'][1],
-                    'upperBound': self._coverage_properties['bbox'][3],
-                    'uomLabel': self._coverage_properties['bbox_units'],
-                    'resolution': self._coverage_properties['resy']
-                }],
-                'gridLimits': {
-                    'type': 'GridLimits',
-                    'srsName': 'http://www.opengis.net/def/crs/OGC/0/Index2D',
-                    'axisLabels': ['i', 'j'],
-                    'axis': [{
-                        'type': 'IndexAxis',
-                        'axisLabel': 'i',
-                        'lowerBound': 0,
-                        'upperBound': self._coverage_properties['width']
-                    }, {
-                        'type': 'IndexAxis',
-                        'axisLabel': 'j',
-                        'lowerBound': 0,
-                        'upperBound': self._coverage_properties['height']
-                    }]
-                }
-            },
-            '_meta': {
-                'tags': self._coverage_properties['tags']
-            }
-        }
-
-        new_axis_name = []
-        new_axis = []
-
-        time_axis = {
-                'type': 'RegularAxis',
-                'axisLabel': 'time',
-                'lowerBound': '',
-                'upperBound': '',
-                'uomLabel': 'hour',
-                'resolution': 24
-            }
-
-        # 15km archive, these time values will never change
-        if '15km' in self.data:
-            if 'APCP-024' in self.data:
-                begin = '2011-04-06T12Z'
-                end = '2012-10-02T12Z'
-            else:
-                begin = '2011-04-06T00Z'
-                end = '2012-10-03T00Z'
-                time_axis['resolution'] = 6
-
-        # 10km  RDPA, begin time is always the same
-        elif '0700cutoff' in self.data:
-            if 'APCP-024-0700' in self.data:
-                begin = '2012-10-03T12Z'
-                end = self.get_end_time_from_file()[-1]
-            elif 'APCP-006-0700' in self.data:
-                begin = '2012-10-03T06Z'
-                end = self.get_end_time_from_file()[-1]
-                time_axis['resolution'] = 6
-
-        # 10km preliminary (0100cutoff) data
-        else:
-            if 'APCP-006-0100' in self.data:
-                time_axis['resolution'] = 6
-            begin, end = self.get_end_time_from_file()
-
-        time_axis['lowerBound'] = begin
-        time_axis['upperBound'] = end
-        new_axis_name.append('time')
-        new_axis.extend([time_axis])
-
-        domainset['generalGrid']['axisLabels'].extend(new_axis_name)
-        domainset['generalGrid']['axis'].extend(new_axis)
-
-        return domainset
-
-    # TODO: remove once pyproj is updated on bionic
-    def get_coverage_rangetype(self, *args, **kwargs):
-        """
-        Provide coverage rangetype
-        :returns: CIS JSON object of rangetype metadata
-        """
-
-        rangetype = {
-            'type': 'DataRecord',
-            'field': []
-        }
-
-        for i, dtype, nodataval in zip(self._data.indexes, self._data.dtypes,
-                                       self._data.nodatavals):
-            LOGGER.debug(f'Determing rangetype for band {i}')
-
-            name, units = None, None
-            if self._data.units[i-1] is None:
-                parameter = _get_parameter_metadata(
-                    self._data.profile['driver'], self._data.tags(i))
-                name = parameter['description']
-                units = parameter['unit_label']
-
-            rangetype['field'].append({
-                'id': i,
-                'type': 'Quantity',
-                'name': name,
-                'encodingInfo': {
-                    'dataType': f'http://www.opengis.net/def/dataType/OGC/0/{dtype}'  # noqa
-                },
-                'nodata': nodataval,
-                'uom': {
-                    'id': f'http://www.opengis.net/def/uom/UCUM/{units}',
-                    'type': 'UnitReference',
-                    'code': units
-                },
-                '_meta': {
-                    'tags': self._data.tags(i)
-                }
-            })
-
-        return rangetype
 
     def query(self, properties=[1], subsets={}, bbox=[],
               datetime_=None, format_='json', **kwargs):
@@ -469,131 +321,43 @@ class RDPAProvider(BaseProvider):
                         LOGGER.debug('Returning data in native format')
                         return memfile.read()
 
-    # TODO: remove once pyproj is updated on bionic
-    def gen_covjson(self, metadata, data):
-        """
-        Generate coverage as CoverageJSON representation
-        :param metadata: coverage metadata
-        :param data: rasterio DatasetReader object
-        :returns: dict of CoverageJSON representation
-        """
-
-        LOGGER.debug('Creating CoverageJSON domain')
-        minx, miny, maxx, maxy = metadata['bbox']
-
-        cj = {
-            'type': 'Coverage',
-            'domain': {
-                'type': 'Domain',
-                'domainType': 'Grid',
-                'axes': {
-                    'x': {
-                        'start': minx,
-                        'stop': maxx,
-                        'num': metadata['width']
-                    },
-                    'y': {
-                        'start': maxy,
-                        'stop': miny,
-                        'num': metadata['height']
-                    }
-                },
-                'referencing': [{
-                    'coordinates': ['x', 'y'],
-                    'system': {
-                        'type': self._coverage_properties['crs_type'],
-                        'id': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'
-                    }
-                }]
-            },
-            'parameters': {},
-            'ranges': {}
-        }
-
-        if metadata['bands'] is None:  # all bands
-            bands_select = range(1, len(self._data.dtypes) + 1)
-        else:
-            bands_select = metadata['bands']
-
-        LOGGER.debug(f'bands selected: {bands_select}')
-        for bs in bands_select:
-            pm = _get_parameter_metadata(
-                self._data.profile['driver'], self._data.tags(bs))
-
-            parameter = {
-                'type': 'Parameter',
-                'description': {
-                    'en': pm['description']
-                },
-                'unit': {
-                    'symbol': pm['unit_label']
-                },
-                'observedProperty': {
-                    'id': pm['observed_property_id'],
-                    'label': {
-                        'en': pm['observed_property_name']
-                    }
-                }
-            }
-
-            cj['parameters'][pm['id']] = parameter
-
-        try:
-            for key in cj['parameters'].keys():
-                cj['ranges'][key] = {
-                    'type': 'NdArray',
-                    # 'dataType': metadata.dtypes[0],
-                    'dataType': 'float',
-                    'axisNames': ['y', 'x'],
-                    'shape': [metadata['height'], metadata['width']],
-                }
-                # TODO: deal with multi-band value output
-                cj['ranges'][key]['values'] = data.flatten().tolist()
-        except IndexError as err:
-            LOGGER.warning(err)
-            raise ProviderQueryError('Invalid query parameter')
-
-        return cj
-
-    # TODO: remove once pyproj is updated on bionic
     def _get_coverage_properties(self):
         """
         Helper function to normalize coverage properties
         :returns: `dict` of coverage properties
         """
 
-        properties = {
-            'bbox': [
-                self._data.bounds.left,
-                self._data.bounds.bottom,
-                self._data.bounds.right,
-                self._data.bounds.top
-            ],
-            'bbox_crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84',
-            'crs_type': 'GeographicCRS',
-            'bbox_units': 'deg',
-            'x_axis_label': 'Long',
-            'y_axis_label': 'Lat',
-            'width': self._data.width,
-            'height': self._data.height,
-            'resx': self._data.res[0],
-            'resy': self._data.res[1],
-            'num_bands': self._data.count,
-            'tags': self._data.tags()
-        }
+        properties = super()._get_coverage_properties()
 
-        if self._data.crs is not None:
-            if self._data.crs.is_projected:
-                bbox_crs = f'http://www.opengis.net/def/crs/OGC/1.3/{self._data.crs.to_epsg()}'  # noqa
-                properties['bbox_crs'] = bbox_crs
-                properties['x_axis_label'] = 'x'
-                properties['y_axis_label'] = 'y'
-                properties['bbox_units'] = self._data.crs.linear_units
-                properties['crs_type'] = 'ProjectedCRS'
+        properties['restime'] = 'YYYY-MM-DDT06Z/PT24H, YYYY-MM-DDT12Z/PT24H'
 
-        properties['axes'] = [
-            properties['x_axis_label'], properties['y_axis_label']
-        ]
+        # 15km archive, these time values will never change
+        if '15km' in self.data:
+            if 'APCP-024' in self.data:
+                begin = '2011-04-06T12Z'
+                end = '2012-10-02T12Z'
+            else:
+                begin = '2011-04-06T00Z'
+                end = '2012-10-03T00Z'
+                properties['restime'] = 'PT6H'
+
+        # 10km  RDPA, begin time is always the same
+        elif '0700cutoff' in self.data:
+            if 'APCP-024-0700' in self.data:
+                begin = '2012-10-03T12Z'
+                end = self.get_end_time_from_file()[-1]
+            elif 'APCP-006-0700' in self.data:
+                begin = '2012-10-03T06Z'
+                end = self.get_end_time_from_file()[-1]
+                properties['restime'] = 'PT6H'
+
+        # 10km preliminary (0100cutoff) data
+        else:
+            if 'APCP-006-0100' in self.data:
+                properties['restime'] = 'PT6H'
+            begin, end = self.get_end_time_from_file()
+
+        properties['time_range'] = [begin, end]
 
         return properties
 
@@ -657,32 +421,3 @@ class RDPAProvider(BaseProvider):
         end = datetime.strptime(end, '%Y%m%d%H').strftime('%Y-%m-%dT%HZ')
 
         return [begin, end]
-
-
-# TODO: remove once pyproj is updated on bionic
-def _get_parameter_metadata(driver, band):
-    """
-    Helper function to derive parameter name and units
-    :param driver: rasterio/GDAL driver name
-    :param band: int of band number
-    :returns: dict of parameter metadata
-    """
-
-    parameter = {
-        'id': None,
-        'description': None,
-        'unit_label': None,
-        'unit_symbol': None,
-        'observed_property_id': None,
-        'observed_property_name': None
-    }
-
-    if driver == 'GRIB':
-        parameter['id'] = band['GRIB_ELEMENT']
-        parameter['description'] = band['GRIB_COMMENT']
-        parameter['unit_label'] = band['GRIB_UNIT']
-        parameter['unit_symbol'] = band['GRIB_UNIT']
-        parameter['observed_property_id'] = band['GRIB_SHORT_NAME']
-        parameter['observed_property_name'] = band['GRIB_COMMENT']
-
-    return parameter
